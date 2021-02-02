30467e6d8aa017fd33112ebb84ecf52edd7cc368
Handy <75829362+handy-perdana-mulya@users.noreply.github.com>
2021-02-02 19:53:51 +0700

    Delete koneksi.php

--- koneksi.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $koneksi = mysqli_connect("localhost", "root", "", "db_mahasiswa");
-
-    if(mysqli_connect_errno($koneksi))
-    {
-        echo "Koneksi Gagal ". mysqli_connect_error();
-    }
-?>
