898d34e0b89fa76445ce47a2962058f7a2fc55c3
Handy <75829362+handy-perdana-mulya@users.noreply.github.com>
2021-02-02 19:55:21 +0700

    Delete delete.php

--- delete.php
+++
@@ -1,6 +0,0 @@
-<?php
-    include "koneksi.php";
-    $id = $_GET['idmahasiswa'];
-    $ambilData = mysqli_query($koneksi, "DELETE FROM mahasiswa WHERE idmahasiswa='$id'");
-    echo "<meta http-equiv='refresh' content='1;url=http://localhost/web-mahasiswa/data_mahasiswa.php'>";
-?>
